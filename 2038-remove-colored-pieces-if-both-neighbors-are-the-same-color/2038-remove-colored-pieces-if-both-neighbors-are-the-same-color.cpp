8ccdd070e4ec973b830f30a989eae5521627a15f
Dhruv Khandelwal <59694554+dhruv21dk@users.noreply.github.com>
2023-10-02 13:36:53 +0530

    Time: 35 ms (47.75%), Space: 13.6 MB (17.42%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -3,41 +3,17 @@ public:
     bool winnerOfGame(string colors) {
         int n = colors.size();
         int cntA=0,cntB=0;
-        int a=0,b=0;
-        for(int i=0;i<n;i++){
-            if(colors[i]=='A'){
-                a++;
-            }else{
-                if(a>=3){
-                    cntA+=a-2;
-                }
-                a = 0;
+        for(int i=1;i<n-1;i++){
+           char curr = colors[i];
+            char prev = colors[i-1];
+            char nex = colors[i+1];
+            if(colors[i]=='A'&&curr==prev&&curr==nex){
+                cntA++;
+            }
+            if(colors[i]=='B'&&curr==prev&&curr==nex){
+                cntB++;
             }
         }
-        for(int i=0;i<n;i++){
-            if(colors[i]=='B'){
-                b++;
-            }else{
-                if(b>=3){
-                    cntB+=b-2;
-                }
-                b = 0;
-            }
-        }
-        if(colors[n-1]=='A'){
-            if(a>=3){
-                cntA+=a-2;
-            }
-        }else{
-            if(b>=3){
-                cntB+=b-2;
-            }
-        }
-        cout<<cntA<<endl;
-        cout<<cntB<<endl;
-        if(cntB>=cntA){
-            return false;
-        }
-        return true;
+        return cntA>cntB;
     }
 };
