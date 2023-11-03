65b2333f08ef5aee5e7fd075c49bda54a860593d
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2023-11-03 08:40:26 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (49.67%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int j = 0;
+        vector<string> ans;
+        for(int i=1;i<=n;i++){
+            if(i==target[j]){
+                ans.push_back("Push");
+                j++;
+            }else{
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+            
+            if(j==target.size()){
+                break;
+            }
+        }
+        return ans;
+    }
+};
