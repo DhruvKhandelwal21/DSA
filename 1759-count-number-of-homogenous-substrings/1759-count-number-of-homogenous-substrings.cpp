3977dd9df12cba5c70d316d9ec9e1bfa248ad1aa
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2023-11-09 23:03:03 +0530

    Time: 24 ms (71.96%), Space: 11.9 MB (85.65%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -5,9 +5,7 @@ public:
         long long mod = 1e9 + 7;
         int cnt = 0;
         int temp = 0;
-        unordered_map<char,int> mp;
         while(j<n){
-            mp[s[j]]++;
             if(s[j]!=s[i]){
                 cnt+=((long long)(temp)*(temp+1)/2)%mod;
                 temp = 0;
