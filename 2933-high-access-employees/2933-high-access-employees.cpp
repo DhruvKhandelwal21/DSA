528e8f44f28588b7336f2be9be7b0d1a34a21adc
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2023-11-17 10:48:24 +0530

    Time: 62 ms (70.31%), Space: 49.2 MB (25.10%) - LeetHub

--- 2933-high-access-employees/2933-high-access-employees.cpp
+++ 2933-high-access-employees/2933-high-access-employees.cpp
@@ -4,8 +4,7 @@ public:
         unordered_map<string,vector<int>> mp;
         vector<string> ans;
         for(auto it:access_times){
-            string temp = it[1];
-            int minutes = stoi(temp.substr(0,2))*60 + stoi(temp.substr(2));
+            int minutes = stoi(it[1]);
             mp[it[0]].push_back(minutes);
         }
         
@@ -15,10 +14,8 @@ public:
                 continue;
             }
             sort(minutes.begin(),minutes.end());
-            int cnt = 1;
-            int diff = 60;
             for(int i=2;i<minutes.size();i++){
-                if(minutes[i] - minutes[i-2]<60){
+                if(minutes[i] - minutes[i-2]<100){
                     ans.push_back(it.first);
                     break;
                 }
