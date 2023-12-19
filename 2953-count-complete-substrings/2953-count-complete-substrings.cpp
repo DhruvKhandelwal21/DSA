d284c36ca76a224085bb85fda526ab7807ee0f16
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2023-12-19 10:58:10 +0530

    Time: 584 ms (62.25%), Space: 185 MB (30.18%) - LeetHub

--- 2953-count-complete-substrings/2953-count-complete-substrings.cpp
+++ 2953-count-complete-substrings/2953-count-complete-substrings.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     bool check(vector<int> &freq,int j,int k){
         int cnt = 0;
+//In this check function we are checking if we are getting all character count equal to k.
         for(int i=0;i<freq.size();i++){
             if(freq[i]==0) continue;
             if(freq[i]!=k) return false;
             cnt++;
         }
     
-        if(cnt>j) return false;
+        // if(cnt>j) return false;
         return true;
     }
     long long solve(string temp,int k){
         int n = temp.size();
         long long cnt = 0;
+//Now we will check for each window length and maintain a vector of characters for keeping the count.
         for(int j=1;j<=26;j++){
             int len = j*k;
             vector<int> freq(26,0);
@@ -41,6 +43,7 @@ public:
         int n = s.size();
         for(int i=0;i<n;i++){
             temp+=s[i];
+//We are first checking the break where the difference between the character is greater than 2. So that we don't have to worry about second condition as characters before that would have difference less than 2.
             if(i<=n-2){
                 if(abs(s[i+1]-s[i])>2){
                     ans+=solve(temp,k);
