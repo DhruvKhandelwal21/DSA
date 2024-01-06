00397899f09c6d8f4df2f24ce4a73c235303909a
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-01-06 15:16:07 +0530

    Time: 532 ms (44.79%), Space: 153.3 MB (19.09%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int solve(vector<int>& nums, int target, int n, vector<vector<int>> &dp){
-        if(target==0){
-            return 0;
-        }
-        if(n<0){
-            return -1e9;
+        if(n<=0){
+            if(target==0){
+                return 0;
+            }else{
+                return -1e9;
+            }
         }
         if(dp[n][target]!=-1){
             return dp[n][target];
         }
         int pick = -1e9;
-        if(target>=nums[n]){
-            pick = 1 + solve(nums,target-nums[n],n-1,dp);
+        if(target>=nums[n-1]){
+            pick = 1 + solve(nums,target-nums[n-1],n-1,dp);
         }
         int nopick = solve(nums,target,n-1,dp);
         return dp[n][target] = max(pick,nopick);
     }
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        int ans = solve(nums,target,n-1,dp);
+        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+        int ans = solve(nums,target,n,dp);
         return ans<=0?-1:ans;
     }
 };
