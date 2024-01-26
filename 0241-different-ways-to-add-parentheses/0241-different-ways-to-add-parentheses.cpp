6874eb65d49eb9ee5c4f2cbc55a495cf01b350e3
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-01-26 23:19:32 +0530

    Time: 5 ms (27.50%), Space: 8.7 MB (75.36%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -6,15 +6,18 @@ public:
         }
         return false;
     }
-    vector<int> solve(string &s,int i,int j){
+    vector<int> solve(string &s,int i,int j,vector<vector<vector<int>>> &dp){
+        if(!dp[i][j].empty()) {
+            return dp[i][j];
+        }
        if(j-i+1<=2){
-           return {stoi(s.substr(i,j-i+1))};
+           return dp[i][j] = {stoi(s.substr(i,j-i+1))};
        }
         vector<int> res;
       for(int idx=i;idx<=j;idx++){
           if(isOperator(s[idx])){
-              vector<int> left = solve(s,i,idx-1);
-              vector<int> right = solve(s,idx+1,j);
+              vector<int> left = solve(s,i,idx-1,dp);
+              vector<int> right = solve(s,idx+1,j,dp);
               for(auto l:left){
                   for(auto r:right){
                       if(s[idx]=='+'){
@@ -28,12 +31,13 @@ public:
               }
           }
       }
-        return res;   
+        return dp[i][j] = res;   
     }
     vector<int> diffWaysToCompute(string expression) {
         vector<int> ans;
         int n = expression.size();
-        ans = solve(expression,0,n-1);
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n));
+        ans = solve(expression,0,n-1,dp);
         return ans;
     }
 };
