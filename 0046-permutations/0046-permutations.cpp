a995d28ddec703ac964c3a1f9281c5c433c005a4
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-02-24 18:50:36 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (45.68%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,32 +1,28 @@
 class Solution
 {
     public:
-        void solve(vector<int> &nums, vector<vector< int >> &ans, unordered_set<int> &st,vector<int> &temp, int n)
+        void solve(vector<int> &nums, vector<vector< int >> &ans, vector<int> &temp, int idx, int n)
         {
-            if (st.size() == n)
+            if (idx == n)
             {
-                ans.push_back(temp);
+                ans.push_back(nums);
                 return;
             }
             
-            for (int i = 0; i < n; i++) {
-                if(st.find(nums[i])==st.end()){
-                    st.insert(nums[i]);
-                    temp.push_back(nums[i]);
-                    solve(nums,ans,st,temp,n);
-                    st.erase(nums[i]);
-                    temp.pop_back();
-                }
+            for (int i = idx; i < n; i++) {
+                   swap(nums[i],nums[idx]);
+                   solve(nums,ans,temp,idx+1,n);
+                   swap(nums[idx],nums[i]);
+                
             }
             return;
         }
     vector<vector < int>> permute(vector<int> &nums)
     {
         int n = nums.size();
-        vector<vector < int>> ans;
+        vector<vector< int >> ans;
         vector<int> temp;
-        unordered_set<int> st;
-        solve(nums, ans, st, temp, n);
+        solve(nums, ans, temp, 0, n);
         return ans;
     }
 };
