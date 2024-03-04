2d19bfcefad4467e7a03c3dc8ccbf716465375e8
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-03-04 07:50:26 +0530

    Time: 1079 ms (12.50%), Space: 125.3 MB (62.50%) - LeetHub

--- 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
+++ 3072-distribute-elements-into-two-arrays-ii/3072-distribute-elements-into-two-arrays-ii.cpp
@@ -14,23 +14,26 @@ public:
             int ele = nums[i];
             int a = temparr1.size();
             int b = temparr2.size();
-            int len1 = temparr1.end() - upper_bound(temparr1.begin(),temparr1.end(),ele);
-            int len2 = temparr2.end() - upper_bound(temparr2.begin(),temparr2.end(),ele);
-            // int len1 = a-idx1;
-            // int len2 = b-idx2;
+            auto idx1 = upper_bound(temparr1.begin(),temparr1.end(),ele);
+            auto idx2 = upper_bound(temparr2.begin(),temparr2.end(),ele);
+            int len1 = a-(idx1-temparr1.begin());
+            int len2 = b-(idx2-temparr2.begin());
             // cout<<idx1<< " "<<idx2<<endl;
+            // cout<<len1<< " "<<len2<<endl;
+            
             if(len1>len2){
                 arr1.push_back(ele);
-                temparr1.insert(temparr1.end() - len1, ele);
+                temparr1.insert(idx1, ele);
             }else if(len1<len2){
                 arr2.push_back(ele);
-                temparr2.insert(temparr2.end() - len2, ele);
+                cout<<temparr2.end() - idx2<<endl;
+                temparr2.insert(idx2, ele);
             }else if(len1 == len2){
                 if(a<=b){
-                    temparr1.insert(temparr1.end() - len1, ele);
+                    temparr1.insert(idx1, ele);
                     arr1.push_back(ele);
                 }else{
-                    temparr2.insert(temparr2.end() - len2, ele);
+                    temparr2.insert(idx2, ele);
                     arr2.push_back(ele);
                 }
             }    
