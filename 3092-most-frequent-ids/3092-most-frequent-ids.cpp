460d068334ebe416ccb99e8f6e804c05bd180470
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-03-24 10:37:26 +0530

    Time: 299 ms (100.00%), Space: 156.5 MB (100.00%) - LeetHub

---
+++ 3092-most-frequent-ids/3092-most-frequent-ids.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<long long> mostFrequentIDs(vector<int>& nums, vector<int>& freq) {
+        map<int,long long> mp;
+        int n = nums.size();
+        vector<long long> res;
+        priority_queue<pair<long long,int>> pq;
+        for(int i=0;i<n;i++){
+            long long ans = INT_MIN;
+            mp[nums[i]]+=freq[i];
+            pq.push({mp[nums[i]],nums[i]});
+            while(!pq.empty()&&mp[pq.top().second]!=pq.top().first){
+                pq.pop();
+            }
+            res.push_back(pq.top().first);
+             
+        }
+        
+        return res;
+    }
+};
