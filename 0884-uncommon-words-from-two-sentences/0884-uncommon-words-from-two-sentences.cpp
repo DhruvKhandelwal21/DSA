6fa9f6146f5ab99bd63aefb59f0d706c68100407
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-09-17 18:28:27 +0530

    Time: 4 ms (40.63%), Space: 8.9 MB (45.08%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -2,50 +2,25 @@ class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string,int> mp1;
-        unordered_map<string,int> mp2;
-        vector<string> st1, st2;
+        string x = s1 + " " + s2;
         int idx = 0;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]==' '){
-                string temp = s1.substr(idx,i-idx);
-                st1.push_back(temp);
+        for(int i=0;i<x.size();i++){
+            if(x[i]==' '){
+                string temp = x.substr(idx,i-idx);
                 mp1[temp]++;
                 idx = i+1;
             }
         }
-        string temp = s1.substr(idx,s1.size()-idx);
-        st1.push_back(temp);
+        string temp = x.substr(idx,s1.size()-idx);
         mp1[temp]++;
         
-        idx = 0;
-        for(int i=0;i<s2.size();i++){
-            if(s2[i]==' '){
-                string temp = s2.substr(idx,i-idx);
-                st2.push_back(temp);
-                mp2[temp]++;
-                idx = i+1;
-            }
-        }
-        string tempx = s2.substr(idx,s2.size()-idx);
-        st2.push_back(tempx);
-        mp2[tempx]++;
-        
         vector<string> ans;
-        int m = st1.size()-1, n = st2.size()-1;
-        while(m>=0 || n>=0){
-            if(m>=0){
-               if(mp2.find(st1[m])==mp2.end() && mp1[st1[m]]<2){
-                   ans.push_back(st1[m]);
-               }
-            m--;
-            }
-            if(n>=0){
-                if(mp1.find(st2[n])==mp1.end() && mp2[st2[n]]<2){
-                    ans.push_back(st2[n]);
-                }
-                n--;
+        for(auto it: mp1){
+            if(it.second==1){
+                ans.push_back(it.first);
             }
         }
+       
       return ans;
     }
 };
