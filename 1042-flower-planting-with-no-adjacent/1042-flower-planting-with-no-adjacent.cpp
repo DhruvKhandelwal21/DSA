2e9dbe51212dfb08a627fbdfcca313e75dca058b
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-10-10 20:32:59 +0530

    Time: 92 ms (72.69%), Space: 48.8 MB (62.36%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -3,51 +3,32 @@ class Solution
     public:
         vector<int> gardenNoAdj(int n, vector<vector < int>> &paths)
         {
-            vector<vector < int>> graph(n + 1);
+            vector<vector < int>> graph(n);
             for (int i = 0; i < paths.size(); i++)
             {
                 int u = paths[i][0];
                 int v = paths[i][1];
-                graph[u].push_back(v);
-                graph[v].push_back(u);
+                graph[u-1].push_back(v-1);
+                graph[v-1].push_back(u-1);
             }
-            vector<int> flower(n + 1, -1);
-            queue<int> q;
-
-            for (int i = 1; i <= n; i++)
-            {
-                q.push(i);
-                if (flower[i] == -1)
-                {
-                    while (!q.empty())
-                    {
-                        int garden = q.front();
-                        q.pop();
-                        vector<int> v(5, -1);
-
-                        for (auto it: graph[garden])
-                        {
-                            if (flower[it] == -1)
-                            {
-                                q.push(it);
-                            }
-                            else
-                            {
-                                v[flower[it]] = 0;
-                            }
-                        }
-                        for (int i = 1; i < 5; i++)
-                        {
-                            if (v[i] == -1)
-                            {
-                                flower[garden] = i;
-                                break;
-                            }
-                        }
-                    }
+            vector<int> ans(n,0);
+            for(int i=0;i<n;i++){
+                if(!ans[i]){
+                 vector<int> used(5,0);
+                  for(auto it: graph[i]){
+                      if(ans[it]!=0){
+                          used[ans[it]] = 1;
+                      }
+                  }
+                  for(int k=1;k<5;k++){
+                      if(!used[k]){
+                          ans[i] = k;
+                          break;
+                      }
+                  }
                 }
             }
-            flower.erase(flower.begin());
-            return flower;
+            return ans;
+
         }
 };
