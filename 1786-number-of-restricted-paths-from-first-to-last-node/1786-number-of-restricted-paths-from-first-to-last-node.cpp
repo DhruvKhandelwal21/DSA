84947b61eb551f34c97f9ace1fccfd7ea2e2370f
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-10-24 14:10:28 +0530

    Time: 99 ms (99.80%), Space: 152.2 MB (76.61%) - LeetHub

---
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    typedef pair<int,int> p;
+    int MOD=1e9+7;
+    int dfs(vector<vector<pair<int,int>>> &graph, vector<int> &dist, vector<int> &dp, int node, int end){
+        long long int ans = 0;
+        if(node==end) return 1;
+        if(dp[node]!=-1)
+            return dp[node];
+        for(auto it: graph[node]){
+            if(it.first!=node && dist[node]>dist[it.first]){
+                    ans= (ans + dfs(graph,dist,dp, it.first, end))%MOD;   
+            }
+        }
+        return dp[node] = ans;
+    }
+    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> graph(n+1);
+        for(int i=0;i<edges.size();i++){
+            int a = edges[i][0];
+            int b = edges[i][1];
+            int c = edges[i][2];
+            graph[a].push_back({b,c});
+            graph[b].push_back({a,c});
+        }
+        priority_queue<p, vector<p>, greater<p>> pq;
+        queue<int> q;
+        q.push(1);
+        pq.push({0,n});
+        vector<int> dist(n+1,INT_MAX);
+        vector<int> vis(n+1,0);
+        dist[n] = 0;
+        while(!pq.empty()){
+            auto temp = pq.top();
+            int node = temp.second;
+            int nodedist = temp.first;
+            pq.pop();
+            if(vis[node]) continue;
+            vis[node] = 1;
+            for(auto it: graph[node]){
+                int adjNode = it.first;
+                int adjDist = it.second;
+                if(dist[adjNode]>nodedist+adjDist){
+                    dist[adjNode] = nodedist+adjDist;
+                    pq.push({dist[adjNode], adjNode});
+                }
+            }
+        }
+     // vector<int> newVis(n+1,0);
+    vector<int> dp(n+1,-1);
+     int ans = dfs(graph, dist,dp, 1, n);
+        
+     // while(!q.empty()){
+     //     int node = q.front();
+     //     q.pop();
+     //     for(auto it: graph[node]){
+     //         if(it.first==n){
+     //             ans++;
+     //             continue;
+     //         }
+     //         if( it.first!=node && dist[node]>dist[it.first]){
+     //             q.push(it.first);
+     //         }
+     //     }
+     // }
+        return ans;
+    }
+};
