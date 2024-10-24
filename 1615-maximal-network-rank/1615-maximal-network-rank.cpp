bcb2cf1de230f9bb402476b4546f0e9f8fe85d61
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-10-24 18:57:11 +0530

    Time: 39 ms (90.36%), Space: 43.1 MB (29.12%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -3,13 +3,13 @@ public:
 
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int> outD(n,0);
-        vector<vector<int>> graph(n);
+        vector<unordered_set<int>> graph(n);
         int ans = 0;
         for(int i=0;i<roads.size();i++){
             int u = roads[i][0];
             int v = roads[i][1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+            graph[u].insert(v);
+            graph[v].insert(u);
             outD[u]++;
             outD[v]++;
         }
@@ -17,12 +17,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                int commonEdge = 0;
-               for(int k=0;k<graph[i].size();k++){
-                   if(graph[i][k]==j){
-                       commonEdge = 1;
-                       break;
-                   }
-               }
+               if(graph[i].count(j)) commonEdge = 1;
                ans = max(ans, outD[i]+outD[j]-commonEdge);
             }
         }
