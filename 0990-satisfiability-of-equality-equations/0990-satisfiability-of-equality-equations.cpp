da84bb5ab14d75c08c08262f833bb7b74d2865ad
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-10-26 00:32:53 +0530

    Time: 3 ms (79.97%), Space: 15.2 MB (62.22%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -1,88 +1,21 @@
 class Solution {
+ int uf[26];
+    int find(int x) {
+        return uf[x] == x ? x : (uf[x] = find(uf[x]));
+    }
 public:
-    class DisjointSet {
-	    vector<int> rank, parent, size;
-	    public:
-	      DisjointSet(int n){
-	          rank.resize(n+1,0);
-	          size.resize(n+1);
-	          parent.resize(n+1);
-	          for(int i=0;i<=n;i++){
-	              parent[i] = i;
-	              size[i] = 1;
-	          }
-	      }
-	      int findUpar(int node){
-	          if(node==parent[node]){
-	              return node;
-	          }
-	          return parent[node] = findUpar(parent[node]);
-	      }
-	      
-	      void unionByRank(int u, int v){
-	          int parU = findUpar(u);
-	          int parV = findUpar(v);
-	          if(parU==parV) return;
-	          
-	          if(rank[parU]>rank[parV]){
-	              parent[parV] = parU;
-	          } else if(rank[parU]<rank[parV]){
-	              parent[parU] = parV;
-	          }else {
-	              parent[parU] = parV;
-	              rank[parV]++;
-	          }
-	      }
-	      
-	      void unionBySize(int u, int v){
-	          int parU = findUpar(u);
-	          int parV = findUpar(v);
-	          if(parU==parV) return;
-	          
-	          if(size[parU]>size[parV]){
-	              parent[parV] = parU;
-	          } else if(size[parU]<size[parV]){
-	              parent[parU] = parV;
-	          }else {
-	              parent[parU] = parV;
-	              size[parV]+=size[parU];
-	          }
-	      }
-	};
     bool equationsPossible(vector<string>& equations) {
-        DisjointSet ds(1000);
-        unordered_map<int,unordered_set<int>> mp;
-        for(int i=0;i<equations.size();i++){
-            string temp = equations[i];
-            int node1 = temp[0]-'a';
-            int node2 = temp[3]-'a';
-        
-            if(temp[1]=='='){
-                ds.unionBySize(node1, node2);
-            }
-            // if(temp[1]=='='){
-            //     if(temp=="e==b"){
-            //         cout<<pNode1<<" "<<pNode2<<endl;
-            //     }
-            // if(mp[pNode1].count(pNode2) || mp[pNode2].count(pNode1) ) return false;
-            //     ds.unionBySize(node1, node2);
-            // }else{
-            //     if(ds.findUpar(node1)==ds.findUpar(node2)){
-            //         return false;
-            //     }else {
-            //         mp[pNode1].insert(pNode2);
-            //         mp[pNode2].insert(pNode1);
-            //     }
-            // }
+        for (int i = 0; i < 26; ++i) uf[i] = i;
+        for (auto e : equations) {
+            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a'); 
         }
-        for(int i=0;i<equations.size();i++){
-            string temp = equations[i];
-            int node1 = temp[0]-'a';
-            int node2 = temp[3]-'a';
-            if(temp[1]=='!'){
-                if(ds.findUpar(node1)==ds.findUpar(node2)) return false;
-            }
+        for (auto e : equations) {
+            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
         }
+        for(auto it: uf){
+        cout<<it<<endl;
+    }
         return true;
     }
+    
 };
