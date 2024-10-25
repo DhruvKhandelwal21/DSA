ad61ec600097ea2d76ffbf7fb04bd2261553b51c
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-10-26 00:42:22 +0530

    Time: 0 ms (100.00%), Space: 18.2 MB (5.20%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -1,21 +1,46 @@
 class Solution {
- int uf[26];
-    int find(int x) {
-        return uf[x] == x ? x : (uf[x] = find(uf[x]));
-    }
 public:
+    class DisjointSet {
+	    
+	    public:
+        vector<int> rank, parent, size;
+	      DisjointSet(int n){
+	          rank.resize(n+1,0);
+	          size.resize(n+1);
+	          parent.resize(n+1);
+	          for(int i=0;i<=n;i++){
+	              parent[i] = i;
+	              size[i] = 1;
+	          }
+	      }
+	      int findUpar(int node){
+	          if(node==parent[node]){
+	              return node;
+	          }
+	          return parent[node] = findUpar(parent[node]);
+	      }
+	};
     bool equationsPossible(vector<string>& equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
-        for (auto e : equations) {
-            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a'); 
+        DisjointSet ds(1000);
+        unordered_map<int,unordered_set<int>> mp;
+        for(int i=0;i<equations.size();i++){
+            string temp = equations[i];
+            int node1 = temp[0]-'a';
+            int node2 = temp[3]-'a';
+        
+            if(temp[1]=='='){
+                // ds.unionBySize(node1, node2);
+                ds.parent[ds.findUpar(node1)] = ds.findUpar(node2);
+            }
         }
-        for (auto e : equations) {
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
+        for(int i=0;i<equations.size();i++){
+            string temp = equations[i];
+            int node1 = temp[0]-'a';
+            int node2 = temp[3]-'a';
+            if(temp[1]=='!'){
+                if(ds.findUpar(node1)==ds.findUpar(node2)) return false;
+            }
         }
-        for(auto it: uf){
-        cout<<it<<endl;
-    }
         return true;
     }
-    
 };
