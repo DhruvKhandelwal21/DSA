60732323d4e3d4f3b0bb63f02b1f4e863a53aeb6
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-11-10 23:12:56 +0530

    Time: 31 ms (97.85%), Space: 42.4 MB (80.08%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,15 +1,15 @@
 class Solution {
-    public static int solve(int i, int j, int [] nums, int [][] dp){
-        if(i>j) return 0;
-        int ans = 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        for(int k=i;k<=j;k++){
-            int coins = nums[k]*nums[i-1]*nums[j+1] + solve(i,k-1, nums, dp) + solve(k+1,j,nums, dp);  
-            ans = Math.max(ans, coins);
-        }
-        return dp[i][j]= ans;
+//     public static int solve(int i, int j, int [] nums, int [][] dp){
+//         if(i>j) return 0;
+//         int ans = 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         for(int k=i;k<=j;k++){
+//             int coins = nums[k]*nums[i-1]*nums[j+1] + solve(i,k-1, nums, dp) + solve(k+1,j,nums, dp);  
+//             ans = Math.max(ans, coins);
+//         }
+//         return dp[i][j]= ans;
         
-    }
+//     }
     public int maxCoins(int[] nums) {
         int n = nums.length;
         int[] newNums = new int[n+2];
@@ -18,12 +18,21 @@ class Solution {
         for(int i=1;i<=n;i++){
             newNums[i] = nums[i-1];
         }
-        int [][] dp = new int[n+1][n+1];
+        int [][] dp = new int[n+2][n+2];
         for (int i = 0; i <= n; i++) {
-          Arrays.fill(dp[i], -1); 
+          Arrays.fill(dp[i], 0); 
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=i;j<=n;j++){
+                int ans = 0;
+                for(int k=i;k<=j;k++){
+                    int coins = newNums[k]*newNums[i-1]*newNums[j+1] + dp[i][k-1]+dp[k+1][j];
+                    ans = Math.max(ans,coins);
+                }
+                dp[i][j] = ans;
+            }
         }
         
-        int ans = solve(1,n,newNums, dp);
-        return ans;
+        return dp[1][n];
     }
 }
