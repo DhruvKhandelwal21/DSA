673778efe650eb6d94af9cf7e1132ccc9d0d9834
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-11-12 22:05:01 +0530

    Time: 2488 ms (28.43%), Space: 41.5 MB (70.29%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static boolean isPal(String s, int i, int j){
+    public static boolean isPal(int i, int j, String s){
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)) return false;
             i++;
@@ -7,37 +7,21 @@ class Solution {
         }
         return true;
     }
-    public static int solve(int i, int j, String s, int [][] dp, int [][] dpPal){
-        if(i==j) return 0;
-
-        if(dpPal[i][j]==-1){
-            dpPal[i][j] = isPal(s,i,j) ? 1 : 0;
-        }
-        if(dpPal[i][j]==1) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans = s.length()-1;
-        for(int k=i;k<j;k++){
-            if(isPal(s,i,k)){
-              int partitions = 1 + solve(k+1,j,s,dp,dpPal);
-              ans = Math.min(ans, partitions);  
-            }
-        }
-        return dp[i][j] = ans;
-    }
     public int minCut(String s) {
         int n = s.length();
-        int j = s.length()-1;
-        int i = 0;
-        int [][] dp = new int[n][n];
-        int [][] dpPal = new int[n][n];
-        for(int x=0;x<n;x++){
-            Arrays.fill(dp[x], -1);
+        int []dp = new int[n+1];
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--){
+            int temp = Integer.MAX_VALUE;
+            for(int j=i;j<n;j++){
+                if(isPal(i,j,s)){
+                    int partitions = 1 + dp[j+1];
+                    temp = Math.min(temp, partitions);
+                }
+            }
+            dp[i] = temp;
         }
-        for(int x=0;x<n;x++){
-            Arrays.fill(dpPal[x], -1);
-        }
-        int ans = solve(i,j,s, dp, dpPal);
-        return ans;
+        return dp[0]-1;
+        
     }
 }
