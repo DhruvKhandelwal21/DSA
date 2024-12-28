548280987a6dd84fc643609dea08ae786d952043
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-12-28 08:01:49 +0530

    Time: 2 ms (41.52%) | Memory: 45.2 MB (18.21%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,24 +1,38 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> ans = new ArrayList<>();
-        int i = 0, n = intervals.length;
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            ans.add(intervals[i]);
-            i++;
-        }
-        System.out.println(i);
-        while (i < n && ( intervals[i][0]<=newInterval[1])) {
-            int x = Math.min(newInterval[0], intervals[i][0]);
-            int y = Math.max(newInterval[1], intervals[i][1]);
-            newInterval[0] = x;
-            newInterval[1] = y;
-            i++;
+        int n = intervals.length;
+        for (int i = 0; i < n; i++) {
+            if (intervals[i][1] < newInterval[0]) {
+                ans.add(intervals[i]);
+            } else if (newInterval[1] >= intervals[i][0]) {
+                int x = Math.min(newInterval[0], intervals[i][0]);
+                int y = Math.max(newInterval[1], intervals[i][1]);
+                newInterval[0] = x;
+                newInterval[1] = y;
+            } else if (newInterval[1] < intervals[i][0]) {
+                ans.add(newInterval);
+                newInterval = intervals[i];
+            }
         }
         ans.add(newInterval);
-        while (i < n && newInterval[1]<intervals[i][0]) {
-            ans.add(intervals[i]);
-            i++;
-        }
+        // while (i < n && intervals[i][1] < newInterval[0]) {
+        // ans.add(intervals[i]);
+        // i++;
+        // }
+        // System.out.println(i);
+        // while (i < n && ( intervals[i][0]<=newInterval[1])) {
+        // int x = Math.min(newInterval[0], intervals[i][0]);
+        // int y = Math.max(newInterval[1], intervals[i][1]);
+        // newInterval[0] = x;
+        // newInterval[1] = y;
+        // i++;
+        // }
+        // ans.add(newInterval);
+        // while (i < n && newInterval[1]<intervals[i][0]) {
+        // ans.add(intervals[i]);
+        // i++;
+        // }
         return ans.toArray(new int[ans.size()][]);
     }
 }
