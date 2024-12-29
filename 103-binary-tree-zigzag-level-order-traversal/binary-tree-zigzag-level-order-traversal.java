60532e075ebf84071a5bca33540e22edc15332a6
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2024-12-29 16:51:27 +0530

    Time: 1 ms (70.53%) | Memory: 42.6 MB (9.87%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -16,29 +16,27 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        if(root==null) return ans;
+        if (root == null)
+            return ans;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         boolean flag = false;
         while (q.isEmpty() == false) {
             int n = q.size();
-            List<Integer> arr = new ArrayList<>();
+            Integer []arr = new Integer[n];
+            Arrays.fill(arr, 0);
             for (int i = 0; i < n; i++) {
                 TreeNode temp = q.poll();
                 if (temp.left != null)
                     q.add(temp.left);
                 if (temp.right != null)
                     q.add(temp.right);
-                arr.add(temp.val);
+                int idx = flag ? n-i-1 : i;
+                arr[idx] = temp.val;
             }
-            if (flag == false) {
-                ans.add(arr);
-                flag = true;
-            } else {
-                Collections.reverse(arr);
-                ans.add(arr);
-                flag = false;
-            }
+            ArrayList<Integer> list = new ArrayList<>(Arrays.asList(arr));
+            ans.add(list);
+            flag = !flag;
         }
         return ans;
     }
