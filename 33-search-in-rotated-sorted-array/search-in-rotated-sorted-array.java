4d122d59659ee72ebe1f2af7b06f83052e0839b9
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2025-01-18 23:30:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (78.79%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,47 @@
+class Solution {
+    int applyBinarySearch(int[] nums, int l, int h, int target) {
+        int lo = l, hi = h;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int lo = 0, hi = nums.length - 1;
+        int smallestIdx = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[lo] <= nums[hi]) {
+                smallestIdx = lo;
+                break;
+            }
+            if (nums[(mid + n - 1) % n] >= nums[mid] && nums[(mid + 1) % n] >= nums[mid]) {
+                smallestIdx = mid;
+                break;
+            }
+            if (nums[mid]>=nums[lo]) {
+                lo = mid + 1;
+            }else if(nums[mid] <= nums[hi]){
+                hi = mid - 1;
+            }
+        }
+    
+        if(nums[smallestIdx]==target) return smallestIdx;
+        int p1 = applyBinarySearch(nums, 0, smallestIdx - 1, target);
+        int p2 = applyBinarySearch(nums, smallestIdx + 1, nums.length - 1, target);
+        if (p1 != -1)
+            return p1;
+        if (p2 != -1)
+            return p2;
+        return -1;
+    }
+}
