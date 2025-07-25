ba061229526967e8280acebecaf5cfe79ddd2b9f
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2025-07-25 22:54:44 +0530

    Time: 2 ms (74.06%) | Memory: 42.1 MB (97.50%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
@@ -2,16 +2,14 @@ class Solution {
     public int maxSum(int[] nums) {
         int ans = 0;
         int n = nums.length;
-        int sum = 0;
-        HashSet<Integer> st = new HashSet<Integer>();
-        for(int i=0;i<n;i++){
-            st.add(nums[i]);
-        }
+        Arrays.sort(nums);
         int mn = Integer.MIN_VALUE;
-        for(Integer i: st){
-            mn = Math.max(mn, i);
-            if(i>=0) ans+=i;
+        for(int i=0;i<n;i++){
+            mn = Math.max(mn, nums[i]);
+            if(nums[i]<0) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            ans+=nums[i];
         }
-        return ans==0 && mn<0 ? mn : ans;
+        return mn<0 ? mn : ans;
     }
 }
