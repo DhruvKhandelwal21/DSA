334571be17ba1c306aeaa2c0024f8ae8ab21080e
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2025-09-02 23:02:35 +0530

    Time: 15 ms (31.34%) | Memory: 44.5 MB (74.63%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        int res = 0;
+        for(int i=0;i<points.length;i++){
+            for(int j=0;j<points.length;j++){
+                if(i==j) continue;
+                if(goodCouple(i, j, points)) res++;
+            }
+        }
+        return res;
+    }
+    public boolean goodCouple(int i, int j, int [][]points){
+        int a = points[i][0];
+        int b = points[i][1];
+        int x = points[j][0];
+        int y = points[j][1];
+        if(a<=x && b>=y && checkEmptyArea(i,j,points)) return true;
+        return false;
+    }
+
+    public boolean checkEmptyArea(int i, int j, int [][]points){
+        int a = points[i][0];
+        int b = points[i][1];
+        int x = points[j][0];
+        int y = points[j][1];
+        for(int k=0;k<points.length;k++){
+            if(k!=i && k!=j){
+                int m = points[k][0];
+                int n = points[k][1];
+                if((m>=a && m<=x) && (n<=b && n>=y)) return false;
+            }
+        }
+        return true;
+    }
+}
