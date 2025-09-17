5b2e80b0bc48ab970204aa5efca2805d0644b961
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2025-09-17 21:47:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.java
+++ Difficulty: Medium/Geek's Training/geeks-training.java
@@ -1,57 +1,27 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            int N = sc.nextInt();
-            int[][] arr = new int[N][3];
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < 3; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-            Solution obj = new Solution();
-            int res = obj.maximumPoints(arr, N);
-            System.out.println(res);
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
-    public int solve(int arr[][], int currIdx, int avt, int n, int [][] dp){
-        if(currIdx>=n) return 0;
-        if(dp[currIdx][avt]!=-1) return dp[currIdx][avt];
-        int ans = Integer.MIN_VALUE;
+    public int solve(int [][]arr, int [][]dp, int idx, int prev){
+        if(idx<0) return 0;
+        if(dp[idx][prev]!=-1) return dp[idx][prev];
+        int ans = 0;
         for(int i=0;i<3;i++){
-            if(i!=avt){
-             int temp = arr[currIdx][i] + solve(arr, currIdx+1, i, n, dp); 
-             ans = Math.max(ans, temp);
+            if(prev!=i){
+                ans = Math.max(ans, arr[idx][i] + solve(arr, dp, idx-1, i));
             }
         }
-        return dp[currIdx][avt]= ans;
+        return dp[idx][prev] = ans;
     }
-    public int maximumPoints(int arr[][], int N) {
-        int ans = Integer.MIN_VALUE;
-        int [][] dp = new int[N][3];
-        for(int i=0;i<N;i++){
+    public int maximumPoints(int arr[][]) {
+        int n = arr.length;
+        int [][]dp = new int[n][3];
+        for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
+        int mx = 0;
         for(int i=0;i<3;i++){
-            ans = Math.max(ans, arr[0][i] + solve(arr,1,i,N, dp));
+            mx = Math.max(mx, arr[n-1][i] + solve(arr, dp, n-2, i));
         }
-        return ans;
+        return mx;
     }
 }
