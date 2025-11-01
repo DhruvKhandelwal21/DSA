1da0827a1aed8d133d219ac3efa07de7d448b8e6
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2025-11-01 20:05:02 +0530

    Time: 1 ms (80.67%) | Memory: 43.4 MB (5.72%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,36 +1,30 @@
 class Solution {
-    public int solve(int[] prices, int buyFlag, int idx, int[][][] dp, int k) {
+    public int solve(int[] prices, int buyFlag, int idx, int[][] dp) {
         if (idx >= prices.length)
             return 0;
 
-        if (dp[idx][buyFlag][k] != -1)
-            return dp[idx][buyFlag][k];
+        if (dp[idx][buyFlag] != -1)
+            return dp[idx][buyFlag];
         int pick = 0, noPick = 0;
-        if (k == 0) {
-            if (buyFlag == 0) {
-                pick = -prices[idx] + solve(prices, 1, idx + 1, dp, k);
-            } else {
-                pick = prices[idx] + solve(prices, 0, idx + 1, dp, 1);
-            }
-        }
-        if (k == 1) {
-            noPick = solve(prices, buyFlag, idx + 1, dp, 0);
+
+        if (buyFlag == 0) {
+            pick = -prices[idx] + solve(prices, 1, idx + 1, dp);
         } else {
-            noPick = solve(prices, buyFlag, idx + 1, dp, k);
+            pick = prices[idx] + solve(prices, 0, idx + 2, dp);
         }
-        return dp[idx][buyFlag][k] = Math.max(pick, noPick);
+
+        noPick = solve(prices, buyFlag, idx + 1, dp);
+        return dp[idx][buyFlag] = Math.max(pick, noPick);
     }
 
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][2];
+        int[][] dp = new int[n][2];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
+            Arrays.fill(dp[i], -1);
         }
 
-        return solve(prices, 0, 0, dp, 0);
+        return solve(prices, 0, 0, dp);
     }
 }
