c8c76d94289bd3cf0595eadb23fcccae615a2124
Dhruv Khandelwal <59694554+DhruvKhandelwal21@users.noreply.github.com>
2025-11-01 18:48:36 +0530

    Time: 3 ms (11.00%) | Memory: 49.5 MB (5.31%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,21 +1,22 @@
 class Solution {
-    public int solve(int []prices, int idx, int buy, int[][]dp){
+    public int solve(int[] prices, int buyFlag, int idx, int [][]dp){
         if(idx>=prices.length) return 0;
-        if(dp[buy][idx]!=-1) return dp[buy][idx];
-        int ans = 0;
-        if(buy==1){
-            ans = Math.max(-prices[idx] + solve(prices, idx+1, 0, dp), solve(prices, idx+1, buy, dp));
+        int pick = 0, noPick = 0;
+        if(dp[idx][buyFlag]!=-1) return dp[idx][buyFlag];
+        if(buyFlag==0){
+            pick = -prices[idx] + solve(prices, 1, idx+1, dp);
         }else{
-            ans = Math.max(prices[idx] + solve(prices, idx+1, 1, dp), solve(prices, idx+1, buy, dp));
-        }
-        return dp[buy][idx] = ans;
+            pick = prices[idx] + solve(prices, 0, idx+1, dp);
+        } 
+        noPick = solve(prices, buyFlag, idx+1, dp);
+        return dp[idx][buyFlag] = Math.max(pick, noPick);
     }
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int [][]dp = new int[2][n];
-        for(int i=0;i<2;i++){
+        int [][]dp = new int[n][2];
+        for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return solve(prices, 0, 1, dp);
+        return solve(prices, 0, 0, dp);
     }
 }
